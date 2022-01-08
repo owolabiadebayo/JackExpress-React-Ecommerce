37161163329e6b5c35beba86302db318764321ca
Owolabi adebayo Stephen <owolabiadebayo78@yahoo.com>
2022-01-08 12:17:45 +0100

    add privacy

--- src/App.js
+++ src/App.js
@@ -1,80 +1,78 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useStateValue } from './components/StateProvider/StateProvider';
-import { auth } from './firebase';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useStateValue } from "./components/StateProvider/StateProvider";
+import { auth } from "./firebase";
 
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Checkout from './components/Checkout/Checkout';
-import Login from './components/Login/Login';
-
-
-import Electronic from './components/AllPhone/jask/Electronicgadget/Electronic';
-import Iphonestore from './components/AllPhone/jask/Iphone/Iphonestore';
-import Phones from './components/AllPhone/jask/Phones';
-import Paystack1 from './components/Checkout/Paystack/paystack';
-
-
+import Electronic from "./components/AllPhone/jask/Electronicgadget/Electronic";
+import Iphonestore from "./components/AllPhone/jask/Iphone/Iphonestore";
+import Phones from "./components/AllPhone/jask/Phones";
+import Paystack1 from "./components/Checkout/Paystack/paystack";
+import Privacy from "./components/privacy";
 
 function App() {
-
   const [{ user }, dispatch] = useStateValue();
 
-  useEffect(() =>{
+  useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         dispatch({
-          type: 'SET_USER',
-          user: authUser
+          type: "SET_USER",
+          user: authUser,
         });
       } else {
         dispatch({
-          type: 'SET_USER',
-          user: null
+          type: "SET_USER",
+          user: null,
         });
       }
     });
 
     return () => {
       unsubscribe();
-    }
-
+    };
   }, []);
 
-  console.log('User is >>>', user);
+  console.log("User is >>>", user);
 
   return (
     <Router>
       <div className="app">
         <Switch>
-           <Route path="/jask/Phone">
+          <Route path="/jask/Phone">
             <Header />
-            <Iphonestore/>
+            <Iphonestore />
           </Route>
-          <Route path = "/jask/payment">
-            <Paystack1/>
+          <Route path="/jask/payment">
+            <Paystack1 />
           </Route>
           <Route path="/jask/Gadget">
             <Header />
-            <Electronic/>
-          </Route>   
+            <Electronic />
+          </Route>
           <Route path="/jask/allphones">
             <Header />
-            <Phones/>
-          </Route>     
+            <Phones />
+          </Route>
           <Route path="/login">
             <Header />
             <Login />
           </Route>
           <Route path="/checkout">
             <Header />
-            <Checkout /> 
+            <Checkout />
           </Route>
-          <Route exact path="/" >
+          <Route exact path="/">
             <Header />
             <Home />
-          </Route> 
+          </Route>
+          <Route exact path="/privacy">
+            <Privacy />
+          </Route>
         </Switch>
       </div>
     </Router>
